9e32c3c24befb33626e52d9c4f0f2ec4f99a3880
seba423 <97098061+seba423@users.noreply.github.com>
2022-01-05 09:12:30 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,12 +4,6 @@ using namespace std;
 //zmienne
 double liczba1;
 double liczba2;
-double liczba3;
-double liczba4;
-double liczba5;
-double liczba6;
-double liczba7;
-double liczba8;
 char znak_operator;
 double wyniki;
 
@@ -18,23 +12,23 @@ double wyniki;
 int main()
 {
     cout << "Kalkulator \n\n" << endl;
-    cin >> liczba1 >>znak_operator >> liczba2 >> znak_operator >> liczba3 >> znak_operator >> liczba4;
+    cin >> liczba1 >>znak_operator >> liczba2;
 
     if(znak_operator =='+')
     {
-        wyniki = liczba1 + liczba2 + liczba3 + liczba4;
+        wyniki = liczba1 + liczba2;
         cout << "Wynik:"<< wyniki<<endl
                                 <<endl;
     }
     else if(znak_operator == '-')
     {
-        wyniki = liczba1 - liczba2 - liczba3 - liczba4;
+        wyniki = liczba1 - liczba2;
         cout << "Wynik:"<< wyniki<<endl
                                 <<endl;
     }
     else if(znak_operator == '*')
     {
-        wyniki = liczba1 * liczba2 * liczba3 * liczba4;
+        wyniki = liczba1 * liczba2;
         cout << "Wynik:"<< wyniki<<endl
                                 <<endl;
     }
@@ -45,13 +39,12 @@ int main()
             cout << "nie mozna dzielic przez zero"<<endl;
             return 0;
         }
-        wyniki = liczba1 / liczba2 / liczba3 / liczba4;
+        wyniki = liczba1 / liczba2;
         cout << "Wynik:"<< wyniki<<endl
                                 <<endl;
     }
     
 
-    system("pause");
      return 0;
 }
 
