d5eaa866804238db264717f4cd35a7e31ffe5bd2
seba423 <97098061+seba423@users.noreply.github.com>
2022-01-20 18:41:24 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,102 +1,94 @@
-#include<iomanip>
 #include <iostream>
 #include <math.h>
-
+#include <iomanip>
+char znak;
 using namespace std;
-//zmienne
-string dzialanie;
-double liczba1;
-double liczba2;
-char znak_operator;
-double wyniki;
-
 
 
 int main()
 {
     cout << "Kalkulator \n\n" << endl;
-    while (true)
+    cout << "+ dodawanie  , - odejmowanie, * mno\276enie , / dzielenie ,^ pot\251gowanie , V pierwiastek drugiego stopnia(kwadratowy), V3 pierwiastek trzeciego stopnia sze\230cienny" << endl;
+    while(true)
     {
-    cout << "wpisz jakie dzialanie chcesz wykonac" << endl;
-    cin >> dzialanie;
 
-    if(dzialanie == "potegi")
+    cout << "Podaj znak dzia\210ania jakie chesz wykona\206" << endl;
+    cin >> znak;
+    if(znak =='^')
     {
-    double liczba, potega;
-    cout << "Podaj liczbe: ";
+     double liczba, potega;
+    cout << "Podaj liczb\251: ";
     cin >> liczba;
-    cout << "Do ktorej potęgi podniesc liczbe " << liczba << "? Wpisz wartosc: ";
+    cout << "Do kt\242rej pot\251gi podnie\230\206 liczb\251 " << liczba << "? Wpisz warto\230\206: ";
     cin >> potega;
-    cout << "pow(" << liczba << "," << potega << ")=" << pow( liczba, potega ) << endl;   
+    cout << "pow(" << liczba << "," << potega << ")=" << pow( liczba, potega ) << endl;       
     }
-    else if (dzialanie == "dodawanie")
+    else if(znak == '+')
     {
-     double liczba1;
+      double liczba1;
      double liczba2;
      double wynik;
-     cout << "podaj 1 liczbe" <<endl;
+     cout << "podaj 1 liczb\251" <<endl;
      cin >> liczba1;
-     cout << "podaj 2 liczbe" <<endl;
+     cout << "podaj 2 liczb\251" <<endl;
      cin >> liczba2;
      wynik = liczba1 + liczba2;
-     cout << wynik << endl;
+     cout << wynik << endl;    
     }
-    else if (dzialanie == "odejmowanie")
+    else if(znak == '-')
     {
-     double liczba1;
+      double liczba1;
      double liczba2;
      double wynik;
-     cout << "podaj 1 liczbe" <<endl;
+     cout << "podaj 1 liczb\251" <<endl;
      cin >> liczba1;
-     cout << "podaj 2 liczbe" <<endl;
+     cout << "podaj 2 liczb\251" <<endl;
      cin >> liczba2;
      wynik = liczba1 - liczba2;
-    cout <<wynik<<endl;
+    cout <<wynik<<endl;   
     }
-     else if (dzialanie == "mnozenie")
+    else if(znak == '/')
+    {
+       double liczba1;
+     double liczba2;
+     double wynik;
+     cout << "podaj 1 liczb\251" <<endl;
+     cin >> liczba1;
+     cout << "podaj 2 liczb\251" <<endl;
+     cin >> liczba2;
+     wynik = liczba1 / liczba2;
+     cout <<wynik<<endl;   
+    }
+    else if(znak == '*')
     {
      double liczba1;
      double liczba2;
      double wynik;
-     cout << "podaj 1 liczbe" <<endl;
+     cout << "podaj 1 liczb\251" <<endl;
      cin >> liczba1;
      cout << "podaj 2 liczbe" <<endl;
      cin >> liczba2;
      wynik = liczba1 * liczba2;
-    cout <<wynik<<endl;
+     cout <<wynik<<endl;    
     }
-     else if (dzialanie == "dzielenie")
-    {
-     double liczba1;
-     double liczba2;
-     double wynik;
-     cout << "podaj 1 liczbe" <<endl;
-     cin >> liczba1;
-     cout << "podaj 2 liczbe" <<endl;
-     cin >> liczba2;
-     wynik = liczba1 / liczba2;
-    cout <<wynik<<endl;
-    }
-    else if (dzialanie == "pierwiastek")
-    {
-    cout << "Ta funkcja liczy pierwiastek drugiego stopnia"<<endl;    
+    else if(znak == 'V')
+    {  
     double a;
-    cout << "podaj liczbe"<<endl;
+    cout << "podaj liczb\251"<<endl;
     cin >> a;    
     cout<<"Pierwiastek z liczby "<<a<<" wynosi: "<<sqrt(a)<<endl;
-    cout<<"Po zaokragleniu do dwoch miejsc po przecinku: "<<
-	setprecision(2)<<fixed<<sqrt(a)<<endl;
+    cout<<"Po zaokr\245gleniu do dw\242ch miejsc po przecinku: "<<
+	setprecision(2)<<fixed<<sqrt(a)<<endl;    
     }
-    else if (dzialanie == "pierwiastek_szescienny")
+    else if(znak == 'V3')
     {
         double liczba;
-        cout << "Podaj liczbe: ";
+        cout << "Podaj liczb\251: ";
         cin >> liczba;
         double wynik = pow( liczba,( 1 / 3.0 ) ); 
-        cout << "Pierwiastek stopnia trzeciego z liczby " << liczba << " to: " << wynik << endl;
+        cout << "Pierwiastek stopnia trzeciego z liczby " << liczba << " to: " << wynik << endl;    
     }
     }
     system("pause");
-     return 0;
-     
+    return 0;
 }
